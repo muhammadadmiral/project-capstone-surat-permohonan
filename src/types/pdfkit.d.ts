5c3a6173a335b55d5d29288f04c4892a6448b57b
muhammadadmiral <admelar15@gmail.com>
2026-01-11 14:35:27 +0700

    feat: add event listeners for data, end, and error to PDFDocument class

--- src/types/pdfkit.d.ts
+++ src/types/pdfkit.d.ts
@@ -16,6 +16,9 @@ declare class PDFDocument implements PDFKit.PDFDocument {
   fontSize(size: number): this;
   text(text: string, options?: Record<string, unknown>): this;
   moveDown(lines?: number): this;
+  on(event: "data", listener: (chunk: Buffer) => void): this;
+  on(event: "end", listener: () => void): this;
+  on(event: "error", listener: (err: Error) => void): this;
   on(event: string, listener: (...args: unknown[]) => void): this;
   end(): void;
 }
